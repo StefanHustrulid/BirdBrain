09770773297c21fd135fd90eaca6263743b2384e
OliveThePuffin <34463898+OliveThePurrin@users.noreply.github.com>
2021-03-29 15:25:20 -0600

    modified:   ArduinoIO/ArduinoIO.ino

--- ArduinoIO/ArduinoIO.ino
+++ ArduinoIO/ArduinoIO.ino
@@ -1,8 +1,35 @@
+int leftMuscle = 0;
+int rightMuscle = 0;
+bool leftSafety = true;
+bool rightSafety = true;
+
 void setup() {
   Serial.begin(9600);
+
+  //Safety Button Inputs
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  attachInterrupt(digitalPinToInterrupt(2), updateSafety, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(3), updateSafety, CHANGE);
+
+  //Muscle Sensor inputs
+  // A0 A1
 }
 
-void loop() {
+void updateSafety() {
+  leftSafety = digitalRead(2);
+  rightSafety = digitalRead(3);
+  if (leftSafety && rightSafety) Serial.println("Safety OFF");
+  else Serial.println("Safety ON");
+}
+
+void loop() { 
   delay(1000);
-  Serial.println("Hello World");
+  
+  leftMuscle = analogRead(A0);
+  rightMuscle = analogRead(A1);
+  
+  Serial.print("Left:\t");Serial.print(leftMuscle);
+  Serial.print("\tRight:\t");Serial.print(rightMuscle);
+  Serial.print("\n");
 }
